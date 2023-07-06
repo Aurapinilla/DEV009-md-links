a1d8b93fe436b55837cc5ef5265dabf684826fc1
Aurapinilla <aura.pinilla12@gmail.com>
2023-07-06 18:40:24 -0500

    Descargué las dependencias del proyecto e hice una paqueta base de la funcion mdLinks

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// La función debe retornar una promesa que resuelva a un arreglo de objetos
+const mdLinks = (path, options) => {
+return new Promise((resolve, reject) => {});
+}
+
 module.exports = () => {
-  // ...
+  mdLinks
 };
