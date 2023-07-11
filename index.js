d17162608cfa8687de8de5f004eeab9bb8ab350a
Aurapinilla <aura.pinilla12@gmail.com>
2023-07-11 18:30:17 -0500

    la función mdLinks verifica si el path existe y lo convierte en absoluto

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+const { log } = require('console');
 const fs = require('fs');
+const path = require('path');
 
 // La función debe retornar una promesa que resuelva a un arreglo de objetos
-const mdLinks = (path, options) => {
+const mdLinks = (filePath) => {
 return new Promise((resolve, reject) => {
   //Si la ruta existe
-  if (fs.existsSync(path)) {
+  if (fs.existsSync(filePath)) {   
 //Convertir la ruta relativa a absoluta
+    resolve(path.resolve(filePath));
 //La ruta absoluta es un directorio o archivo?
 //Si es directorio extraer el archivo md
-    resolve();
   } else {
     //Si la ruta no existe rechaza la promesa
     reject('Error: La ruta no existe');
   }
 });
 };
+console.log(mdLinks("Librerias.md"));
 
 module.exports = {
   mdLinks
