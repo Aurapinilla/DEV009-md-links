34bb3df63365b26b1e59fb9979b5d801abb48b29
Aurapinilla <aura.pinilla12@gmail.com>
2023-07-11 20:08:25 -0500

    La función devuelve un array con los links que encuentra en el documento .md

--- Librerias.md
+++ Librerias.md
@@ -11,14 +11,14 @@ Express es un framework web rápido, minimalista y flexible para Node.js. Es una
 ***Tutorial de Express en Node.js.dev:*** https://nodejs.dev/learn/build-an-express-app
 
 
-###Axios
+### Axios
 
 Axios es una popular librería de JavaScript utilizada para realizar solicitudes HTTP desde el navegador o desde Node.js. Proporciona una interfaz sencilla y intuitiva para realizar peticiones a servidores y trabajar con respuestas.
 
 ***Repositorio oficial de Axios en GitHub:*** https://github.com/axios/axios
 ***Documentación de Axios:*** (Link Inválido) https://axios-http.com/docs/introd
 
-###Jest
+### Jest
 
 Jest es una popular librería de pruebas unitarias para JavaScript. Está diseñada para facilitar la escritura de pruebas automáticas, fomentar las mejores prácticas de pruebas y proporcionar una experiencia de desarrollo ágil.
 
--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
+const links = require('markdown-link-extractor');
+const axios = require('axios');
 
 // La función debe retornar una promesa que resuelva a un arreglo de objetos
 const mdLinks = (filePath) => {
-return new Promise((resolve, reject) => {
-  //Si la ruta existe
-  if (fs.existsSync(filePath)) {   
-//Convertir la ruta relativa a absoluta
-    resolve(path.resolve(filePath));
-//La ruta absoluta es un directorio o archivo?
-//Si es directorio extraer el archivo md
-  } else {
-    //Si la ruta no existe rechaza la promesa
-    reject('Error: La ruta no existe');
-  }
-});
+  return new Promise((resolve, reject) => {
+    //Si la ruta existe
+    if (fs.existsSync(filePath)) {
+      //Convertir la ruta relativa a absoluta
+      const absPath = path.resolve(filePath)
+      resolve(absPath);
+      //La ruta absoluta es un directorio o archivo?
+      //Si es directorio extraer el archivo sólo si es extensión md
+    } else {
+      //Si la ruta no existe rechaza la promesa
+      reject('Error: La ruta no existe');
+    }
+  })
+  //Leer el archivo encontrado
+  .then((absPath) => {
+    return fs.promises.readFile(absPath, 'utf8');
+  })
+  //Extraer los links encontrados
+  .then((data) => {
+    return links(data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
 };
-console.log(mdLinks("Librerias.md"));
+
+//Leer el archivo encontrado
+/*mdLinks
+  .then((result) => {
+    fs.readFile(absPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('No se pudo leer el archivo');
+      }
+      else {
+        return data;
+      }
+    });
+  });*/
+
+
+  mdLinks("Librerias.md")
+  .then((result) => {
+    log(result);
+  });
 
 module.exports = {
   mdLinks
