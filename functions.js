2b651375847649b9e0f0db17033034e9b31d2c39
Aurapinilla <aura.pinilla12@gmail.com>
2023-07-14 12:02:14 -0500

    En el archivo funciones.js está la secuencia de funciones desde el momento inicial de validar el path, hasta extraer los links

--- functions.js
+++ functions.js
@@ -13,25 +13,47 @@ const pathExists = (filePath) => {
 //console.log(pathExists("README.js"));
 
 //If path belongs to a folder, only extract .md files
+const obtainMdFiles = (absPath) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(absPath, (err, files) => {
+            if (err) {
+                reject(new Error('Not able to read the folder'));
+            } else {
+                const archivosMd = files.filter(file => absPath.extname(file) === '.md');
+                if (archivosMd.length === 0) {
+                    reject(new Error('No ".md" files found'));
+                } else {
+                    resolve(readMdFile(mdFile));
+                }
+            }
+        });
+    });
+};
+
 
 //Read File
-function readMdFile(absPath) {
+const readMdFile = (mdFile) => {
     return new Promise((resolve, reject) => {
-        fs.readFile(absPath, 'utf8', (err, data) => {
-            const fileExt = path.extname(absPath);
-            if (fileExt === '.md') {
-                resolve(extractLinks(data, absPath));
-            }
-            else {
-                reject(new Error('This is not a Markdown File'));
+        fs.readFile(mdFile, 'utf8', (err, data) => {
+            if (err) {
+                reject(new Error('Not able to read the file'))
+            } else {
+                const fileExt = path.extname(mdFile);
+                if (fileExt === '.md') {
+                    resolve(extractLinks(data, mdFile));
+                }
+                else {
+                    reject(new Error('This is not a Markdown File'));
 
+                }
             }
+
         });
 
     });
 };
 //Extract Links from a file
-function extractLinks (data, absPath) {
+const extractLinks = (data, mdFile) => {
     const regex = /\[(.+?)\]\((https?:\/\/[^\s]+)\)/g;
     const linksObj = [];
     let match;
@@ -39,21 +61,21 @@ function extractLinks (data, absPath) {
     while ((match = regex.exec(data))) {
         const href = match[2];
         const text = match[1];
-        linksObj.push({ href, text, file: absPath });
+        linksObj.push({ href, text, file: mdFile });
     }
 
     return linksObj;
 };
 
 readMdFile("librerias.md")
-  .then((links) => {
-    console.log(links); // Aquí se muestra el resultado completo de la promesa
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+    .then((links) => {
+        console.log(links); // Aquí se muestra el resultado completo de la promesa
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 
 
 module.exports = {
-    pathExists, extractLinks, readFilemd: readMdFile
+    pathExists, readMdFile, obtainMdFiles
 };
